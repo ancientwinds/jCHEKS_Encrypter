79d9385026901edabbe906e845e8385fc8b55615
frgagnon <frgagnon@ste-foy.qc.ca>
2015-05-19 21:38:10 -0400

    Project initialization

---
+++ src/com/archosResearch/jCHEKS/encrypter/RinjdaelEncrypter.java
@@ -0,0 +1,15 @@
+package com.archosResearch.jCHEKS.encrypter;
+
+import com.archosResearch.jCHEKS.concept.encrypter.AbstractEncrypter;
+
+/**
+ *
+ * @author fgagnon
+ */
+public class RinjdaelEncrypter extends AbstractEncrypter{
+
+    @Override
+    public void encrypt() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+}
