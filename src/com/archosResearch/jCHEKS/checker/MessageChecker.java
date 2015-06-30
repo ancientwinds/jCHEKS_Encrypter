7b4bebba0785fb03f6a394809572d2bbf9023929
LepageT <thomas.lepage@hotmail.ca>
2015-06-30 09:54:32 -0400

    MessageChecker class created.

---
+++ src/com/archosResearch/jCHEKS/checker/MessageChecker.java
@@ -0,0 +1,16 @@
+package com.archosResearch.jCHEKS.checker;
+
+/**
+ *
+ * @author Thomas Lepage thomas.lepage@hotmail.ca
+ */
+public class MessageChecker {
+    
+    public static String encodeMessageCheck(byte[] key, String message) {
+        return null;
+    }
+    
+    public static String decodeMessageCheck(byte[] key, String message) {
+        return null;
+    }
+}
