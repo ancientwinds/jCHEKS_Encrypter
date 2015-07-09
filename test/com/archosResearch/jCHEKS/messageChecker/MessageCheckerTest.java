970cc8a5122ed4a921ebe8b61d6078d2f38e3270
LepageT <thomas.lepage@hotmail.ca>
2015-07-09 10:35:12 -0400

    Clean up in the test for MessageChecker and SecureAckGenerator.

--- test/com/archosResearch/jCHEKS/messageChecker/MessageCheckerTest.java
+++ test/com/archosResearch/jCHEKS/messageChecker/MessageCheckerTest.java
@@ -2,23 +2,18 @@ package com.archosResearch.jCHEKS.messageChecker;
 
 import com.archosResearch.jCHEKS.concept.chaoticSystem.AbstractChaoticSystem;
 import com.archosResearch.jCHEKS.encrypter.mock.MockChaoticSystem;
-import com.archosResearch.jCHEKS.messageChecker.exception.MessageCheckerException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author etudiant
+ * @author Thomas Lepage thomas.lepage@hotmail.ca
  */
 public class MessageCheckerTest {
 
     private static final String message = "MessageChecker test.";
     private static final String messageChecked = "DyVN0oWKkxgbuJrk50Xrq3Nh8+lDUkTBJwYffVbFEZs=";
     
-    /**
-     * Test of encodeMessage method, of class MessageChecker.
-     * @throws java.lang.Exception
-     */
     @Test
     public void testEncodeMessage() throws Exception {
         AbstractChaoticSystem chaoticSystem = new MockChaoticSystem(MessageChecker.getKeyLength());
@@ -26,9 +21,6 @@ public class MessageCheckerTest {
         assertEquals(result, messageChecked);
     }
 
-    /**
-     * Test of validateMessage method, of class MessageChecker.
-     */
     @Test
     public void testValidateMessage() throws Exception {
         AbstractChaoticSystem chaoticSystem = new MockChaoticSystem(MessageChecker.getKeyLength());
