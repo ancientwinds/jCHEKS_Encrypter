1c084c9e77aae3c699a99b2710814adb03eac5ad
Michael Roussel <rousselm4@gmail.com>
2015-08-03 11:05:54 -0400

    Fix mock.

--- test/com/archosResearch/jCHEKS/encrypter/mock/MockChaoticSystem.java
+++ test/com/archosResearch/jCHEKS/encrypter/mock/MockChaoticSystem.java
@@ -37,6 +37,11 @@ public class MockChaoticSystem extends AbstractChaoticSystem{
     }
 
     @Override
+    public boolean isSameState(AbstractChaoticSystem system) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
     public void resetSystem() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
